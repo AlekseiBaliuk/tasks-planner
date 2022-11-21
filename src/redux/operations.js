2bf43888aa52edc10190d0ca2d64e661196eecfb
Aleksei Baliuk <aleksred.b@gmail.com>
2022-11-21 22:39:44 +0200

    upd

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://637b70f56f4024eac20da688.mockapi.io';
 
-// axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
-
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchAll',
   async (_, thunkAPI) => {
